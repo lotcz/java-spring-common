620d2ae69daa4976a3610079acfea492ebff455d
Karel Zavadil <zavadil@incomaker.com>
2025-03-17 16:56:56 +0100

    added http client

---
+++ src/main/java/eu/zavadil/java/spring/common/client/HttpApiClientBase.java
@@ -0,0 +1,87 @@
+package eu.zavadil.java.spring.common.client;
+
+import eu.zavadil.java.UrlBuilder;
+import eu.zavadil.java.caching.Lazy;
+import eu.zavadil.java.util.ExceptionUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+public class HttpApiClientBase {
+
+	protected final String baseUrl;
+
+	protected final Lazy<HttpHeaders> defaultHeaders;
+
+	public HttpApiClientBase(String baseUrl) {
+		this.baseUrl = baseUrl;
+		this.defaultHeaders = new Lazy<>(
+			() -> {
+				HttpHeaders headers = new HttpHeaders();
+				headers.add("Content-Type", "application/json");
+				return headers;
+			}
+		);
+	}
+
+	public HttpHeaders getHttpHeaders(String path) {
+		return this.defaultHeaders.get();
+	}
+
+	protected String getUrl(String path, Map<String, String> queryParams) {
+		UrlBuilder builder = UrlBuilder.of(this.baseUrl);
+		builder.addPath(path);
+		if (queryParams != null) {
+			queryParams.forEach(builder::addQuery);
+		}
+		return builder.buildAsString();
+	}
+
+	protected <T> HttpEntity<T> createHttpEntity(String path, T obj) {
+		return new HttpEntity<T>(obj, this.getHttpHeaders(path));
+	}
+
+	protected <TReq, TRes> TRes exchange(HttpMethod method, String path, Map<String, String> queryParams, TReq request, Class<TRes> cls) {
+		String url = this.getUrl(path, queryParams);
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			ResponseEntity<TRes> response = restTemplate.exchange(url, method, this.createHttpEntity(path, request), cls);
+			try {
+				TRes result = response.getBody();
+				if (response.getStatusCode().isError()) {
+					throw new RuntimeException(String.format("Http error %d: %s", response.getStatusCode().value(), result));
+				}
+				return result;
+			} catch (Exception e) {
+				throw new RuntimeException("Cannot get body from response!", e);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(
+				String.format(
+					"Error when invoking remote endpoint %s: %s",
+					url,
+					ExceptionUtils.getMessage(e)
+				),
+				e
+			);
+		}
+	}
+
+	protected <TReq, TRes> TRes exchange(HttpMethod method, String path, TReq request, Class<TRes> responseClass) {
+		return this.exchange(method, path, null, request, responseClass);
+	}
+
+	protected <TRes> TRes get(String path, Map<String, String> queryParams, Class<TRes> responseClass) {
+		return this.exchange(HttpMethod.GET, path, queryParams, null, responseClass);
+	}
+
+	protected <TRes> TRes get(String path, Class<TRes> responseClass) {
+		return this.get(path, null, responseClass);
+	}
+
+}
